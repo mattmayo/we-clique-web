b8624bf71469747be2bafeccc019f3a4e6eeec67
Matt Mayo <matt_mayo@icloud.com>
2019-08-30 09:00:54 -0700

    Fix styles

--- pages/index.js
+++ pages/index.js
@@ -2,42 +2,60 @@ import React from "react";
 import Head from "next/head";
 import Container from "@material-ui/core/Container";
 import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/styles";
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-      />
-    </Head>
-    <Container maxWidth="xs">
-      <div className="container">
+const useStyles = makeStyles({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)"
+  },
+  containerChild: {
+    margin: "16px"
+  }
+});
+
+const Home = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </Head>
+      <Container className={classes.container} maxWidth="xs">
+        <Typography
+          align="center"
+          className={classes.containerChild}
+          variant="h4"
+        >
+          We Clique
+        </Typography>
         <TextField
           autoFocus={true}
+          className={classes.containerChild}
           label="email"
           required={true}
           variant="outlined"
         />
         <TextField
           autoFocus={true}
+          className={classes.containerChild}
           label="password"
           required={true}
           type="password"
           variant="outlined"
         />
-      </div>
-    </Container>
-
-    <style jsx>{`
-      .container {
-        display: flex;
-        flex-direction: column;
-        text-align: center;
-      }
-    `}</style>
-  </div>
-);
+      </Container>
+    </div>
+  );
+};
 
 export default Home;
